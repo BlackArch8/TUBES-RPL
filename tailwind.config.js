7fc7a3bab0eefc772eca787c2c21ef99e290ef9f
MuhammadAldiRivandi <6182001029@student.unpar.ac.id>
2023-11-25 17:20:30 +0700

    config tailwind, login, daftar(status), daftar(datadiri), daftar(upload)

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    flex: {
+      '2': '2 2 0%',
+      '1.6': '1.6 1.6 0%'
+    },
+    fontFamily: {
+      'lato': ['Lato'],
+      'poppins': ['Poppins']
+    },
+  },
+  plugins: [],
+}
+
