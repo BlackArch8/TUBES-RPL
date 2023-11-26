829803055739a4f121816f152de36077a5975098
Doni Andrian <91331154+BlackArch8@users.noreply.github.com>
2023-11-27 00:02:30 +0700

    update index js comment

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ app.use('/login', LoginRoute);
 
 //register routes
 import {RegisterRoute, DataDiriRoute, UploadNilaiRoute} from "./routes/Register.js";
-// Step 1: Choose Status
+
+//status
 app.use('/register/isi-status', RegisterRoute);
 
-// Step 2: Fill Personal Information
+//data diri
 app.use('/register/data-diri', DataDiriRoute);
 
-// Step 3: Upload Nilai
+//upload nilai
 app.use('/register/upload-nilai',UploadNilaiRoute); 
 app.listen(8080, () => {
     console.log("Server started on port 8080");
