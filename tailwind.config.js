74cba7c72a460edf6380ef42f2bb46d95d5869fa
gunawanwinata17 <96789079+gunawanwinata17@users.noreply.github.com>
2023-11-29 23:38:31 +0700

    Modifikasi overlay sama perbaikin tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./views/**/*.{html,js}"],
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
-    flex: {
-      '2': '2 2 0%',
-      '1.6': '1.6 1.6 0%'
-    },
-    fontFamily: {
-      'lato': ['Lato'],
-      'poppins': ['Poppins']
-    },
   },
   plugins: [],
 }
