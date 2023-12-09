0571435f1f10ba3063febb7606dd5b6ccba2f436
Doni Andrian <91331154+BlackArch8@users.noreply.github.com>
2023-12-10 00:33:04 +0700

    update routing register, dan save nilai pdf

--- index.js
+++ index.js
@@ -6,15 +6,33 @@ import session from "express-session";
 import path from "path";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
+import validator from "validator";
+import multer from "multer";
+
 
 
 const app = express();
 const staticPath = path.resolve("public");
 app.use('/public', express.static(staticPath));
+app.use("/Nilai_data", express.static("Nilai_data"));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+
+//connect database
+import {db} from "./database/database.js";
+
+//middleware
+const auth = (req, res, next) => {
+  if (req.session.npm) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
+
+
 
 //login route
 import {LoginRoute} from "./routes/Login.js";
@@ -22,18 +40,74 @@ import {LoginRoute} from "./routes/Login.js";
 app.use('/', LoginRoute);
 
 //register routes
-import {RegisterRoute, DataDiriRoute, UploadNilaiRoute, UploadJadwalMatkul} from "./routes/Register.js";
+import {RegisterRoute, DataDiriRoute,DataDiriAlumniRoute, UploadNilaiRoute, UploadNilaiAlumniRoute, UploadJadwalMatkul, UploadJadwalMatkulAlumni} from "./routes/Register.js";
 
 //status
 app.use('/register/isi-status', RegisterRoute);
 
+
 //data diri
 app.use('/register/data-diri', DataDiriRoute);
+let data_diri = [];
+app.post('/register/data-diri', (req, res) => {
+  data_diri.push({
+    npm: req.body.npm,
+    nama: req.body.namalengkap,
+    email: req.body.email,
+    jumlah_matkul: req.body.jumlah_matkul,
+  });
+
+  console.log(data_diri);
+
+  res.redirect("/register/upload-nilai");
+});
+
+//data diri alumni
+app.use('/register/data-diri-alumni', DataDiriAlumniRoute);
+app.post('/register/data-diri-alumni', (req, res) => {
+  data_diri.push({
+    npm: req.body.npwp,
+    nama: req.body.namalengkap,
+    email: req.body.email,
+    jumlah_matkul: req.body.jumlah_matkul,
+  });
+
+  console.log(data_diri);
+
+  res.redirect("/register/upload-nilai-alumni");
+});
+
+//multer
+const filestorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./Nilai_data");
+  },
+  filename: (req, file, cb) => {
+    //ganti nama filenya ke npm
+    cb(null, data_diri[data_diri.length-1].npm + ".pdf");
+  },
+});
+
+const upload = multer({ storage: filestorage });
+
+//upload nilai
+app.use('/register/upload-nilai',UploadNilaiRoute); 
+app.post('/register/upload-nilai', upload.single("nilai"), (req, res) => {
+  console.log(req.file);
+  res.redirect("/register/matakuliah");
+});
+
+app.use('/register/upload-nilai-alumni',UploadNilaiAlumniRoute);
+app.post('/register/upload-nilai-alumni', upload.single("nilai"), (req, res) => {
+  console.log(req.file);
+  res.redirect("/register/matakuliah-alumni");
+});
 
 //mata kuliah
 app.use('/register/matakuliah', UploadJadwalMatkul);
+app.use('/register/matakuliah-alumni', UploadJadwalMatkulAlumni);
 
-//koordinator 
+//koordinator routes
 import {DashBoardRoute, PenugasanRoute, SeleksiRoute, ListAsdosRoute, JadwalRoute, TambahMatkulRoute, SettingRoute} from "./routes/Koordinator/Dashboard.js";
 
 app.use('/koordinator/dashboard', DashBoardRoute);
@@ -88,8 +162,7 @@ app.post("/api/send", async (req, res) => {
   res.send("Email sent successfully");
 });
 
-//upload nilai
-app.use('/register/upload-nilai',UploadNilaiRoute); 
+
 app.listen(8080, () => {
     console.log("Server started on port 8080");
   });
