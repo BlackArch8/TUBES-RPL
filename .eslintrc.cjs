4f5dc5b8f30bf6191b5b85e55e2dc2f70d81c3b3
Andrew-FebPr <6182001048@student.unpar.ac.id>
2023-12-18 12:56:46 +0700

    Menambahkan ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
     "env": {
+        "browser": true,
         "es2021": true,
         "node": true
     },
@@ -22,6 +23,5 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-        "no-unused-vars": "off",
     }
 }
