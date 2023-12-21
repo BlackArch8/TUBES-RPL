23f01b7e64cd414a9d5a7365ad78e9bd6e7169a5
gunawanwinata17 <96789079+gunawanwinata17@users.noreply.github.com>
2023-12-21 07:53:30 +0700

    Nambahin unit testing tapi belom 100 persen fix, just in case save dulu aja

---
+++ test/RoutesDosen.test.js
@@ -0,0 +1,33 @@
+import request from 'supertest';
+import express from 'express';
+import { DosenRoute } from '../routes/Dosen/RoutesDosen'; 
+
+const app = express();
+app.use('/', DosenRoute);
+
+describe('Testing Dosen Routes', () => {
+  it('should get Dosen dashboard', async () => {
+    const res = await request(app).get('/dosen/dashboard-dosen');
+    expect(res.statusCode).toBe(200); 
+  });
+
+  it('should get Dosen jadwal', async () => {
+    const res = await request(app).get('/dosen/jadwal-dosen');
+    expect(res.statusCode).toBe(200);
+    
+  });
+
+  it('should get Dosen input-asisten', async () => {
+    const res = await request(app).get('/dosen/input-asisten');
+    expect(res.statusCode).toBe(200);
+   
+  });
+
+  it('should get Dosen setting', async () => {
+    const res = await request(app).get('/dosen/setting-dosen');
+    expect(res.statusCode).toBe(200);
+   
+  });
+
+ 
+});
